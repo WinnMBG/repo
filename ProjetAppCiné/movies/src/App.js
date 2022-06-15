e4618e7da3b16e4caf718884147b3244f2e00822
WinnMBG <winn77.m@hotmail.com>
2022-06-15 18:39:46 +0200

    Update

--- ProjetAppCiné/movies/src/App.js
+++ ProjetAppCiné/movies/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <footer>
+        <ul>
+          <li>BONZAIIII!</li>
+          <li>NARUTOOOO</li>
+          <li>BONZAIIII!</li>
+          <li>BONZAIIII!</li>
+          <li>BONZAIIII!</li>
+          <li>BONZAIIII!</li>
+          <li>BONZAIIII!</li>
+        </ul>
+      </footer>
     </div>
   );
 }
 
-export default App;
+export default App
