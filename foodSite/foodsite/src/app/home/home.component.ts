6da567cd98c70ffa089214e4d5c38e94325ed043
WinnMBG <winn77.m@hotmail.com>
2024-10-03 17:25:03 +0200

    daamn

--- foodSite/foodsite/src/app/home/home.component.ts
+++ foodSite/foodsite/src/app/home/home.component.ts
@@ -2,23 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { NgFor } from '@angular/common';
 import { Food } from '../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
 
   foods: Food[] = []
-  constructor(private foodService: FoodService) {
+  constructor(private foodService: FoodService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
-   this.foods = this.foodService.getAll()
+    this.route.params.subscribe(params => {
+      if (params['searchTerm']) {
+        this.foods = this.foodService.getAll().filter(food => food.name.toLowerCase().includes(params['searchTerm'].toLowerCase()))
+      } else {
+        this.foods = this.foodService.getAll()
+      }
+    })
   }
 
 }
